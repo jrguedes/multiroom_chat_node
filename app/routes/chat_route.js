7433953f8c46f4e21176a6fde73007cdc3f2b817
jrguedes <jrguedes.ja@gmail.com>
2020-03-24 11:00:27 -0300

    Arquivos estaticos e configuracao das primeiras rotas

---
+++ app/routes/chat_route.js
@@ -0,0 +1,8 @@
+module.exports = function(application){
+    application.post('/chat', function(req, res){
+        res.render('chat');
+    });
+    application.get('/chat', function(req, res){
+        res.render('chat');
+    });
+}
